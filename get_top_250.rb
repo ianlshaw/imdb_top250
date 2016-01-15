881ad3c0ab73a4c359d18d65f85aec777f64379b
Ian Shaw <ian.shaw@LONICTM003.local>
2016-01-15 17:02:42 +0000

    comments

--- get_top_250.rb
+++ get_top_250.rb
@@ -9,9 +9,14 @@ DOWNLOADS_PATH='/Users/ian.shaw/Downloads'
 def get_top_250()
   #remove the existing version
   File.delete("current_top250.lst")
-  
+
+  #Collect the page html source
   page = Nokogiri::HTML(open("http://www.imdb.com/chart/top"))
+
+  #Return only elements which are part of the titleColumn css class.
   results = page.css('.titleColumn')
+
+  #Loop through each row found.
   results.each do |result|
     result.css('i').each do |i|
       i.content = ''
@@ -19,12 +24,16 @@ def get_top_250()
 
   #remove lines with no content
   no_blanks = result.text.gsub /^$\n/, ''
+
   #remove a bunch of blank space at the beginning of each line
   without_lead = no_blanks.gsub(/\n/,'')[14..-1]
+
   #removes any leading whitespace
   without_wlead = without_lead.lstrip.chop
+
   #removes trailing whitespace
   without_trail = without_wlead.sub(/\s+\Z/, "")
+
   #Removes the last word of each line
   without_year = without_trail[/(.*)\s/,1]
   
@@ -38,12 +47,15 @@ end
 def get_current_inventory
   #Get a list of video type files from the #{DOWNLOADS_PATH}
   inventory = `file --mime-type  #{DOWNLOADS_PATH}/*  | grep video  | sed -e 's/\.[^.]*$//' -e 's@.*/@@'`
+
+  #Write that to a file named inventory.lst
   output = File.new("inventory.lst", "w")
   output.syswrite(inventory)
   output.close
 end
 
 def compare_inventory_to_top250
+  #Compare the two files we've created, by shelling out to bash and using the diff tool
   result = `diff current_top250.lst inventory.lst`
   puts result
 end
